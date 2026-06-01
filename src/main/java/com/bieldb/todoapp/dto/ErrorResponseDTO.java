a9693afd4bbd78c7a16c3a9f8d2ba789b2fb661f
bieldb <gabriel.oliveira02@souunit.com.br>
2026-06-01 11:59:21 -0300

    feat: add task service and exception handling for task not found

---
+++ src/main/java/com/bieldb/todoapp/dto/ErrorResponseDTO.java
@@ -0,0 +1,14 @@
+package com.bieldb.todoapp.dto;
+
+public class ErrorResponseDTO {
+
+    private String message;
+
+    public ErrorResponseDTO(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
--- src/main/java/com/bieldb/todoapp/repository/TaskRepository.java
+++ src/main/java/com/bieldb/todoapp/repository/TaskRepository.java
@@ -1,9 +1,17 @@
 package com.bieldb.todoapp.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bieldb.todoapp.model.Task;
+import com.bieldb.todoapp.model.TaskStatus;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findByStatus(TaskStatus status);
+
+    List<Task> findByDataLimiteBefore(LocalDate hoje);
 
 }
